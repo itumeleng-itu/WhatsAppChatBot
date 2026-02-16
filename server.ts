7807db55974c5bd79ae437216d010d21bc6b8ed1
itumeleng-itu <matomempaola@gmail.com>
2026-02-16 11:06:01 +0200

    feat: install project dependencies and packages

---
+++ server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import 'dotenv/config';
+
+const app = express();
+const PORT: number = Number(process.env.PORT) || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.listen(PORT, (): void => {
+  console.log(` Server running on  https://localhost:${PORT}`);
+});
